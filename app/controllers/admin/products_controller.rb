1f3b8c3ddf312e77a7350b4f90d02ae9f0233367
beranemo <beranemo@gmail.com>
2018-09-13 22:05:13 +0800

    create basic view

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,30 @@
 class Admin::ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+  end
+  
+  def new
+    @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    
+    if @product.save
+      flash[:notice] = "商品資料已成功更新"
+      redirect_to admin_products_path
+    else
+      render :index
+    end
+  end
+  
+   def product_params
+    params.require(:product).permit(
+      :title,
+      :price,
+      :image
+      )
+  end
+  
 end
